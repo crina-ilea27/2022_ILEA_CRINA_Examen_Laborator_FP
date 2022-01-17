ec257c565c7c39982bd1e4f66f61adf3466ca5a2
crina-ilea27 <91911683+crina-ilea27@users.noreply.github.com>
2022-01-17 14:01:26 +0200

    2022__ILEA__CRINA_Problema_65

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, z, prod = 1, ok = 0;
+            n = int.Parse(Console.ReadLine());
+            while(n>1)
+            {
+                z = n % 10;
+                if(z%2==1)
+                {
+                    prod = prod * z;
+                    ok = 1;
+                }
+                n = n / 10;
+            }
+            if(ok==0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(prod);
+            }
+            Console.ReadKey();
+        }
+    }
+}
